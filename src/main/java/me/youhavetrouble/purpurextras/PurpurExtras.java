3a6ae1d530569baf8e1158da6a9ce39bcc9b7191
YouHaveTrouble <youhavetrouble@youhavetrouble.me>
2021-12-21 19:42:15 +0100

    new purpur package and website

--- src/main/java/me/youhavetrouble/purpurextras/PurpurExtras.java
+++ src/main/java/me/youhavetrouble/purpurextras/PurpurExtras.java
@@ -20,12 +20,11 @@ public final class PurpurExtras extends JavaPlugin {
     @Override
     public void onEnable() {
         try {
-            Class.forName("net.pl3x.purpur.PurpurConfig");
+            Class.forName("org.purpurmc.purpur.PurpurConfig");
         } catch (ClassNotFoundException e) {
             getLogger().warning( ChatColor.translateAlternateColorCodes('&', "&x&8&0&5&2&8&0PurpurExtras was created to compliment Purpur, and it appears you're not using it!"));
             getLogger().warning( ChatColor.translateAlternateColorCodes('&', "&x&8&0&5&2&8&0Purpur is a drop-in replacement for " + getServer().getName()+"."));
-            getLogger().warning( ChatColor.translateAlternateColorCodes('&', "&x&8&0&5&2&8&0You can get Purpur on https://purpur.pl3x.net/downloads/"));
-            getServer().getPluginManager().disablePlugin(this);
+            getLogger().warning( ChatColor.translateAlternateColorCodes('&', "&x&8&0&5&2&8&0You can get Purpur on https://purpurmc.org/downloads"));
             return;
         }
 
