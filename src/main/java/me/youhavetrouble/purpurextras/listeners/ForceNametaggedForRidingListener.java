cb17d3daad8be4e58e525206f2eb62a6612bc034
YouHaveTrouble <youhavetrouble@youhavetrouble.me>
2021-12-31 22:48:43 +0100

    fix force nametagged rideables bug

--- src/main/java/me/youhavetrouble/purpurextras/listeners/ForceNametaggedForRidingListener.java
+++ src/main/java/me/youhavetrouble/purpurextras/listeners/ForceNametaggedForRidingListener.java
@@ -1,7 +1,7 @@
 package me.youhavetrouble.purpurextras.listeners;
 
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
+import io.papermc.paper.event.entity.EntityMoveEvent;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -12,8 +12,30 @@ public class ForceNametaggedForRidingListener implements Listener {
     @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
     public void onMount(EntityMountEvent event) {
         Entity entity = event.getMount();
+
+        // if pig or strider with a saddle, ignore
+        if (entity instanceof Steerable steerable && steerable.hasSaddle()) return;
+
+        // if mountable in vanilla, ignore
+        if (entity instanceof Vehicle && (!(entity instanceof Steerable))) return;
+
+        // if player, ignore
         if (entity.getType().equals(EntityType.PLAYER)) return;
+
+        // if has nametag, ignore
         if (entity.getCustomName() != null) return;
+
+        // otherwise don't allow mounting
+        event.setCancelled(true);
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+    public void onMountMove(EntityMoveEvent event) {
+        if (!(event.getEntity() instanceof Vehicle vehicle)) return;
+        if (vehicle.getPassengers().isEmpty()) return;
+        Entity passenger = vehicle.getPassengers().get(0);
+        if (!(passenger instanceof Player player)) return;
+        if (vehicle.getCustomName() != null && player.hasPermission("allow.ride."+vehicle.getType().getKey().getKey())) return;
         event.setCancelled(true);
     }
 
