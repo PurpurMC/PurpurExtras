ccf25f323b58d67dc3a1372df981bf449747ae42
12emin34 <macanovic.emin@gmail.com>
2022-05-04 19:54:21 +0200

    add priority, ignoreCancelled and move check up

--- src/main/java/me/youhavetrouble/purpurextras/listeners/SkeletonWitherRoseListener.java
+++ src/main/java/me/youhavetrouble/purpurextras/listeners/SkeletonWitherRoseListener.java
@@ -7,17 +7,18 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Skeleton;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 
 public class SkeletonWitherRoseListener implements Listener {
-    @EventHandler
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
     public void onRightClick(PlayerInteractEntityEvent event) {
         Entity entity = event.getRightClicked();
+        if (!(entity instanceof Skeleton)) return;
         Location location = entity.getLocation();
         Player player = event.getPlayer();
-        if (!(entity instanceof Skeleton)) return;
         if (!player.getInventory().getItemInMainHand().getType().equals(Material.WITHER_ROSE)) return;
         player.swingMainHand();
         entity.remove();
