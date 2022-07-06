3106ab4eee559f8fae688a47e733c0a61a371e10
IllogicalSong <45906780+illogicalsong@users.noreply.github.com>
2022-07-06 10:34:51 -0700

    ignore cancelled and event priority

--- src/main/java/me/youhavetrouble/purpurextras/listeners/ItemFrameListener.java
+++ src/main/java/me/youhavetrouble/purpurextras/listeners/ItemFrameListener.java
@@ -4,13 +4,14 @@ import com.destroystokyo.paper.event.entity.EntityAddToWorldEvent;
 import org.bukkit.Material;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.EquipmentSlot;
 
 public class ItemFrameListener implements Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
     public void onItemFrameInteract(PlayerInteractEntityEvent event){
         Player player = event.getPlayer();
         Entity entity = event.getRightClicked();
