4a54d5152c785c34b5824262ea27bc607854011b
IllogicalSong <45906780+illogicalsong@users.noreply.github.com>
2022-07-05 22:43:55 -0700

    Less ugly and convoluted

--- src/main/java/me/youhavetrouble/purpurextras/listeners/DispenserCauldronListener.java
+++ src/main/java/me/youhavetrouble/purpurextras/listeners/DispenserCauldronListener.java
@@ -8,7 +8,6 @@ import org.bukkit.block.data.Levelled;
 import org.bukkit.block.data.type.Dispenser;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Arrays;
@@ -16,10 +15,17 @@ import java.util.List;
 
 public class DispenserCauldronListener implements Listener {
     //I know there's already a dispenser listener that listens for this event but I couldn't figure out how on earth to put it in without messing it up :D
+    BlockData waterCauldron = Bukkit.createBlockData(Material.WATER_CAULDRON, blockData -> {
+        Levelled cauldron = (Levelled) blockData;
+        cauldron.setLevel(3);
+    });
+    BlockData powderSnowCauldron = Bukkit.createBlockData(Material.POWDER_SNOW_CAULDRON, blockData -> {
+        Levelled cauldron = (Levelled) blockData;
+        cauldron.setLevel(3);
+    });
+    BlockData lavaCauldron = Bukkit.createBlockData(Material.LAVA_CAULDRON);
     List<Material> cauldronTypes = Arrays.asList(Material.CAULDRON, Material.LAVA_CAULDRON, Material.WATER_CAULDRON, Material.POWDER_SNOW_CAULDRON);
     List<Material> bucketTypes = Arrays.asList(Material.BUCKET, Material.LAVA_BUCKET, Material.WATER_BUCKET, Material.POWDER_SNOW_BUCKET);
-    SoundCategory blockSound = SoundCategory.BLOCKS;
-    Sound dispenseSound = Sound.BLOCK_DISPENSER_DISPENSE;
 
     @EventHandler
     public void onPreDispense(BlockPreDispenseEvent event) {
@@ -27,138 +33,83 @@ public class DispenserCauldronListener implements Listener {
         Dispenser dispenser = (Dispenser) event.getBlock().getBlockData();
         org.bukkit.block.Dispenser blockDispenser = (org.bukkit.block.Dispenser) event.getBlock().getState(false);
         Block block = event.getBlock().getRelative(dispenser.getFacing());
-        Inventory dispenserInventory = blockDispenser.getInventory();
-        int emptySlot = dispenserInventory.firstEmpty();
-        Levelled cauldronBlock;
-        BlockData waterCauldron = Bukkit.createBlockData(Material.WATER_CAULDRON, blockData -> {
-            Levelled cauldron = (Levelled) blockData;
-            cauldron.setLevel(3);
-        });
-        BlockData powderSnowCauldron = Bukkit.createBlockData(Material.POWDER_SNOW_CAULDRON, blockData -> {
-            Levelled cauldron = (Levelled) blockData;
-            cauldron.setLevel(3);
-        });
-        Location dispenserLocation = blockDispenser.getLocation();
-        Location cauldronLocation = block.getLocation();
-        Sound lavaDispense = Sound.ITEM_BUCKET_EMPTY_LAVA;
-        Sound lavaEmpty = Sound.ITEM_BUCKET_FILL_LAVA;
-        Sound waterDispense = Sound.ITEM_BUCKET_EMPTY;
-        Sound waterEmpty = Sound.ITEM_BUCKET_FILL;
-        Sound powderSnowDispense = Sound.ITEM_BUCKET_EMPTY_POWDER_SNOW;
-        Sound powderSnowEmpty = Sound.ITEM_BUCKET_FILL_POWDER_SNOW;
-        Material dispensedItemMaterial = event.getItemStack().getType();
-        ItemStack dispensedItem = event.getItemStack();
-        Material material = block.getType();
-
-        if(!(cauldronTypes.contains(block.getType()))) return;
-        if(!(bucketTypes.contains(dispensedItemMaterial))) return;
-        switch (material){
-            case CAULDRON:
-                if(dispensedItemMaterial.equals(Material.BUCKET)){
-                    return;
-                } else {
-                    switch (dispensedItemMaterial) {
-                        case LAVA_BUCKET -> {
-                            block.setType(Material.LAVA_CAULDRON);
-                            dispensedItem.setType(Material.BUCKET);
-                            playSounds(dispenserLocation, lavaDispense);
-                            event.setCancelled(true);
-                            return;
-                        }
-                        case WATER_BUCKET -> {
-                            block.setBlockData(waterCauldron);
-                            dispensedItem.setType(Material.BUCKET);
-                            playSounds(dispenserLocation, waterDispense);
-                            event.setCancelled(true);
-                            return;
-                        }
-                        case POWDER_SNOW_BUCKET -> {
-                            block.setBlockData(powderSnowCauldron);
-                            dispensedItem.setType(Material.BUCKET);
-                            playSounds(dispenserLocation, powderSnowDispense);
-                            event.setCancelled(true);
-                            return;
-                        }
-                    }
-                }
-
-            case LAVA_CAULDRON:
-                if(!(dispensedItemMaterial.equals(Material.BUCKET))) return;
-                block.setType(Material.CAULDRON);
-                if(dispensedItem.getAmount() > 1) {
-                    ItemStack lavaBucket = new ItemStack(Material.LAVA_BUCKET);
-                    dispensedItem.setAmount(dispensedItem.getAmount() - 1);
-                    if (emptySlot == -1) {
-                        block.getWorld().dropItem(cauldronLocation, lavaBucket);
-                        playSounds(dispenserLocation, lavaEmpty);
-                        event.setCancelled(true);
-                        return;
-                    }
-                    event.setCancelled(true);
-                    playSounds(dispenserLocation, lavaEmpty);
-                    blockDispenser.getInventory().setItem(emptySlot, lavaBucket);
-                    return;
-                }
+        ItemStack dispensedItems = event.getItemStack();
+        Material dispensedItemType = dispensedItems.getType();
+        Material blockType = block.getType();
+        Levelled cauldronLevel;
+        if(!cauldronTypes.contains(blockType)) return;
+        if(!bucketTypes.contains(dispensedItems.getType())) return;
+        if(block.getType().equals(Material.CAULDRON)){
+            if(dispensedItemType.equals(Material.BUCKET)) return;
+            if(dispensedItemType.equals(Material.LAVA_BUCKET)){
+                emptyCauldronHandler(block, lavaCauldron, dispensedItems, blockDispenser.getLocation(), Sound.ITEM_BUCKET_EMPTY_LAVA);
                 event.setCancelled(true);
-                playSounds(dispenserLocation, lavaEmpty);
-                dispensedItem.setType(Material.LAVA_BUCKET);
                 return;
-            case WATER_CAULDRON:
-                if(!(dispensedItemMaterial.equals(Material.BUCKET))) return;
-                cauldronBlock = (Levelled) block.getBlockData();
-                if(cauldronBlock.getLevel() < 3) {
-                    event.setCancelled(true);
-                    return;
-                }
-                block.setType(Material.CAULDRON);
-                if(dispensedItem.getAmount() > 1){
-                    ItemStack waterBucket = new ItemStack(Material.WATER_BUCKET);
-                    dispensedItem.setAmount(dispensedItem.getAmount() - 1);
-                    if(emptySlot == -1){
-                        block.getWorld().dropItem(cauldronLocation, waterBucket);
-                        playSounds(dispenserLocation, waterEmpty);
-                        event.setCancelled(true);
-                        return;
-                    }
-                    event.setCancelled(true);
-                    playSounds(dispenserLocation, waterEmpty);
-                    blockDispenser.getInventory().setItem(emptySlot, waterBucket);
-                    return;
-                }
+            }
+            if (dispensedItemType.equals(Material.WATER_BUCKET)){
+                emptyCauldronHandler(block, waterCauldron, dispensedItems, blockDispenser.getLocation(), Sound.ITEM_BUCKET_EMPTY);
                 event.setCancelled(true);
-                playSounds(dispenserLocation, waterEmpty);
-                dispensedItem.setType(Material.WATER_BUCKET);
                 return;
-            case POWDER_SNOW_CAULDRON:
-                if(!(dispensedItemMaterial.equals(Material.BUCKET))) return;
-                cauldronBlock = (Levelled) block.getBlockData();
-                if(cauldronBlock.getLevel() < 3) {
-                    event.setCancelled(true);
-                    return;
-                }
-                block.setType(Material.CAULDRON);
-                if(dispensedItem.getAmount() > 1){
-                    ItemStack powderSnowBucket = new ItemStack(Material.POWDER_SNOW_BUCKET);
-                    dispensedItem.setAmount(dispensedItem.getAmount() - 1);
-                    if(emptySlot == -1){
-                        block.getWorld().dropItem(cauldronLocation, powderSnowBucket);
-                        playSounds(dispenserLocation, powderSnowEmpty);
-                        event.setCancelled(true);
-                        return;
-                    }
-                    event.setCancelled(true);
-                    playSounds(dispenserLocation, powderSnowEmpty);
-                    blockDispenser.getInventory().setItem(emptySlot, powderSnowBucket);
-                    return;
-                }
+            }
+            if(dispensedItemType.equals(Material.POWDER_SNOW_BUCKET)){
+                emptyCauldronHandler(block, powderSnowCauldron, dispensedItems, blockDispenser.getLocation(), Sound.ITEM_BUCKET_EMPTY_POWDER_SNOW);
                 event.setCancelled(true);
-                playSounds(dispenserLocation, powderSnowEmpty);
-                dispensedItem.setType(Material.POWDER_SNOW_BUCKET);
+                return;
+            }
+        }
+        if(!dispensedItemType.equals(Material.BUCKET)) return;
+        if(block.getType().equals(Material.LAVA_CAULDRON)){
+            fullCauldronHandler(block, dispensedItems, blockDispenser, Material.LAVA_BUCKET, Sound.ITEM_BUCKET_FILL_LAVA);
+            event.setCancelled(true);
+            return;
+        }
+        if(block.getType().equals(Material.WATER_CAULDRON)){
+            cauldronLevel = (Levelled) block.getBlockData();
+            if (cauldronLevel.getLevel() < 3) return;
+            fullCauldronHandler(block, dispensedItems, blockDispenser, Material.WATER_BUCKET, Sound.ITEM_BUCKET_FILL);
+            event.setCancelled(true);
+            return;
+        }
+        if(block.getType().equals(Material.POWDER_SNOW_CAULDRON)){
+            cauldronLevel = (Levelled) block.getBlockData();
+            if (cauldronLevel.getLevel() < 3) return;
+            fullCauldronHandler(block, dispensedItems, blockDispenser, Material.POWDER_SNOW_BUCKET, Sound.ITEM_BUCKET_FILL_POWDER_SNOW);
+            event.setCancelled(true);
         }
     }
-    public void playSounds(Location dLoc, Sound actionSound){
-        dLoc.getWorld().playSound(dLoc, dispenseSound, blockSound, 1, 1);
-        dLoc.getWorld().playSound(dLoc, actionSound, blockSound, 1, 1);
+
+    public void emptyCauldronHandler(Block cauldron,
+                                     BlockData cauldronType,
+                                     ItemStack dispensingItem,
+                                     Location dispLocation,
+                                     Sound uniqueSound){
+        cauldron.setBlockData(cauldronType);
+        dispensingItem.setType(Material.BUCKET);
+        dispLocation.getWorld().playSound(dispLocation, Sound.BLOCK_DISPENSER_DISPENSE, SoundCategory.BLOCKS, 1, 1);
+        dispLocation.getWorld().playSound(dispLocation, uniqueSound, SoundCategory.BLOCKS, 1, 1);
+    }
+
+    public void fullCauldronHandler(Block cauldron,
+                                    ItemStack items,
+                                    org.bukkit.block.Dispenser dispenserBlock,
+                                    Material newItem,
+                                    Sound uniqueSound){
+        cauldron.setType(Material.CAULDRON);
+        ItemStack newItemDrop = new ItemStack(newItem);
+        int emptySlot = dispenserBlock.getInventory().firstEmpty();
+        if(items.getAmount() > 1){
+            items.setAmount(items.getAmount() - 1);
+            if(emptySlot == -1){
+                cauldron.getWorld().dropItem(cauldron.getLocation(), newItemDrop);
+            } else {
+                dispenserBlock.getInventory().setItem(emptySlot, newItemDrop);
+            }
+        } else {
+            items.setType(newItem);
+        }
+        dispenserBlock.getWorld().playSound(dispenserBlock.getLocation(), Sound.BLOCK_DISPENSER_DISPENSE, SoundCategory.BLOCKS, 1, 1);
+        dispenserBlock.getWorld().playSound(dispenserBlock.getLocation(), uniqueSound, SoundCategory.BLOCKS, 1, 1);
     }
 }
+
 
