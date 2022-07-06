d210ed72074d5ffd1f2fdc2958b2e7eb9d28b4a1
IllogicalSong <45906780+illogicalsong@users.noreply.github.com>
2022-07-06 10:33:30 -0700

    event handler priority & ignorecancelled

--- src/main/java/me/youhavetrouble/purpurextras/listeners/NetherRoofBuildListener.java
+++ src/main/java/me/youhavetrouble/purpurextras/listeners/NetherRoofBuildListener.java
@@ -2,12 +2,13 @@ package me.youhavetrouble.purpurextras.listeners;
 
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 
 public class NetherRoofBuildListener implements Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
     public void onNetherRoofBuild(BlockPlaceEvent event){
         Block block = event.getBlock();
         if(!(block.getWorld().hasCeiling())) return;
