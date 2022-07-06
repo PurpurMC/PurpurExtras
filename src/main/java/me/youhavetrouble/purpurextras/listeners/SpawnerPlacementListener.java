9f39fa4456aea849fa23565ca13658a5c97ad548
IllogicalSong <45906780+illogicalsong@users.noreply.github.com>
2022-07-06 10:36:21 -0700

    Event handler priority and ignorecancelled

--- src/main/java/me/youhavetrouble/purpurextras/listeners/SpawnerPlacementListener.java
+++ src/main/java/me/youhavetrouble/purpurextras/listeners/SpawnerPlacementListener.java
@@ -5,12 +5,13 @@ import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 
 public class SpawnerPlacementListener implements Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
     public void onSpawnerPlace(BlockPlaceEvent event){
         Material block = event.getBlock().getType();
         Player player = event.getPlayer();
