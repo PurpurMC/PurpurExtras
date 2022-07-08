3ad23d9679ada69336b15e72e32fb9a9a1f04c9d
IllogicalSong <45906780+illogicalsong@users.noreply.github.com>
2022-07-07 17:09:22 -0700

    specific permissions per entity

--- src/main/java/me/youhavetrouble/purpurextras/listeners/SpawnerPlacementListener.java
+++ src/main/java/me/youhavetrouble/purpurextras/listeners/SpawnerPlacementListener.java
@@ -3,22 +3,29 @@ package me.youhavetrouble.purpurextras.listeners;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Material;
+import org.bukkit.block.CreatureSpawner;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 
+import java.util.Locale;
+
 public class SpawnerPlacementListener implements Listener {
 
     @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
     public void onSpawnerPlace(BlockPlaceEvent event){
-        Material block = event.getBlock().getType();
         Player player = event.getPlayer();
-        if (!(block.equals(Material.SPAWNER))) return;
-        if (!(player.hasPermission("purpurextras.spawnerPlace"))){
-            player.sendMessage(Component.text("You do not have permission to place spawners", NamedTextColor.RED));
+        String entityType;
+        if (event.getBlock().getState(false) instanceof CreatureSpawner spawner) {
+            entityType = ("." + spawner.getSpawnedType()).toLowerCase(Locale.ROOT);
+        } else {
+            return;
+        }
+        if (!(player.hasPermission("purpurextras.spawnerplace" + entityType) || player.hasPermission("purpurextras.spawnerplace.everything"))){
             event.setCancelled(true);
+            player.sendMessage(Component.text("You do not have permission to place a " + spawner.getSpawnedType().toString().toLowerCase(Locale.ROOT) + " spawner!", NamedTextColor.RED));
         }
     }
 }
