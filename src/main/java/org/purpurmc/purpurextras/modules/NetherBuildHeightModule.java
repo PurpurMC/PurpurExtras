eefb1649c0c6959bbb776cd8574bef2125258473
YouHaveTrouble <youhavetrouble@youhavetrouble.me>
2022-07-22 17:38:20 +0200

    codestyle, use already instanciated minimessage, make permission register regardless if feature is on

--- src/main/java/org/purpurmc/purpurextras/modules/NetherBuildHeightModule.java
+++ src/main/java/org/purpurmc/purpurextras/modules/NetherBuildHeightModule.java
@@ -1,6 +1,5 @@
 package org.purpurmc.purpurextras.modules;
 
-import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import org.bukkit.World;
 import org.bukkit.block.Block;
@@ -33,11 +32,11 @@ public class NetherBuildHeightModule implements PurpurExtrasModule, Listener {
     public void enable() {
         PurpurExtras plugin = PurpurExtras.getInstance();
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
-        registerPermission(netherBuildHeightBypassPermission, "Allows player to bypass the configured max nether build height", PermissionDefault.OP);
     }
 
     @Override
     public boolean shouldEnable() {
+        registerPermission(netherBuildHeightBypassPermission, "Allows player to bypass the configured max nether build height", PermissionDefault.OP);
         return PurpurExtras.getPurpurConfig().getBoolean("settings.block-building-above-nether.enabled", false);
     }
 
@@ -45,14 +44,13 @@ public class NetherBuildHeightModule implements PurpurExtrasModule, Listener {
     public void onNetherRoofBuild(BlockPlaceEvent event){
         Block block = event.getBlock();
         Player player = event.getPlayer();
-        if(!(block.getWorld().getEnvironment().equals(World.Environment.NETHER))) return;
-        if(block.getLocation().getBlockY() < configBuildHeight) return;
-        if(player.hasPermission(netherBuildHeightBypassPermission)) return;
-        if(noPermissionMessage){
-            player.sendActionBar(MiniMessage.miniMessage().deserialize(noPermissionMessageContent, Placeholder.unparsed("height", String.valueOf(configBuildHeight))));
+        if (!(block.getWorld().getEnvironment().equals(World.Environment.NETHER))) return;
+        if (block.getLocation().getBlockY() < configBuildHeight) return;
+        if (player.hasPermission(netherBuildHeightBypassPermission)) return;
+        if (noPermissionMessage) {
+            player.sendActionBar(PurpurExtras.getInstance().miniMessage.deserialize(noPermissionMessageContent, Placeholder.unparsed("height", String.valueOf(configBuildHeight))));
         }
         event.setCancelled(true);
-
     }
 
 }
