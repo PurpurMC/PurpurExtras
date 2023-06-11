6f30519a1fca063618dce8e5879681b19a8d9257
youhavetrouble <youhavetrouble@youhavetrouble.me>
2023-06-12 00:28:56 +0200

    clean up unused and deprecated code

--- src/main/java/org/purpurmc/purpurextras/PurpurExtras.java
+++ src/main/java/org/purpurmc/purpurextras/PurpurExtras.java
@@ -1,16 +1,13 @@
 package org.purpurmc.purpurextras;
 
-import org.purpurmc.purpurextras.command.FancyCommand;
 import org.purpurmc.purpurextras.modules.PurpurExtrasModule;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.NamespacedKey;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
-
 
 public final class PurpurExtras extends JavaPlugin {
 
@@ -23,10 +20,10 @@ public final class PurpurExtras extends JavaPlugin {
         try {
             Class.forName("org.purpurmc.purpur.PurpurConfig");
         } catch (ClassNotFoundException e) {
-            getLogger().warning(ChatColor.translateAlternateColorCodes('&', "&x&8&0&5&2&8&0PurpurExtras was created to compliment Purpur, and it appears you're not using it!"));
-            getLogger().warning(ChatColor.translateAlternateColorCodes('&', "&x&8&0&5&2&8&0Purpur is a drop-in replacement for " + getServer().getName() + "."));
-            getLogger().warning(ChatColor.translateAlternateColorCodes('&', "&x&8&0&5&2&8&0You can get Purpur on https://purpurmc.org/downloads"));
-            this.getPluginLoader().disablePlugin(this);
+            getLogger().warning("PurpurExtras was created to compliment Purpur, and it appears you're not using it!");
+            getLogger().warning("Purpur is a drop-in replacement for " + getServer().getName() + ".");
+            getLogger().warning("You can get Purpur on https://purpurmc.org/downloads");
+            this.getServer().getPluginManager().disablePlugin(this);
             return;
         }
 
@@ -36,7 +33,6 @@ public final class PurpurExtras extends JavaPlugin {
         PluginCommand command = getCommand("purpurextras");
         if (command != null) {
             command.setExecutor(new PurpurExtrasCommand());
-            getServer().getPluginManager().registerEvents(new FancyCommand(), this);
         }
 
         PurpurExtrasModule.reloadModules();
