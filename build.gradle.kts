d7e23bd703e0208f54debcbcdde8e5f6034e0fda
TheRealRyGuy <git@ryguy.me>
2023-06-14 21:02:27 -0400

    use latest build api route

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,8 @@
-import org.json.JSONObject
 import java.net.URL
 import java.nio.file.Files
 
 val serverDir: File = projectDir.resolve("run")
 val pluginDir: File = serverDir.resolve("plugins")
-
-buildscript {
-    dependencies {
-        classpath("org.json:json:20090211")
-    }
-}
 
 plugins {
     `java-library`
@@ -86,9 +79,7 @@ tasks {
         doFirst {
             serverDir.mkdirs()
             pluginDir.mkdirs()
-            val obj = JSONObject(URL("https://api.purpurmc.org/v2/purpur/1.20.1").readText(Charsets.UTF_8))
-            val latest = obj.getJSONObject("builds").getInt("latest")
-            URL("https://api.purpurmc.org/v2/purpur/1.20.1/${latest}/download").openStream().use {
+            URL("https://api.purpurmc.org/v2/purpur/1.20.1/latest/download").openStream().use {
                 Files.copy(it, serverDir.resolve("server.jar").toPath())
             }
         }
