214ab35c5c7f2a0a7ada0c70e8e929db667292c0
youhavetrouble <youhavetrouble@youhavetrouble.me>
2023-06-18 12:44:14 +0200

    create suspicious blocks

---
+++ src/main/java/org/purpurmc/purpurextras/modules/CreateSusBlocksModule.java
@@ -0,0 +1,69 @@
+package org.purpurmc.purpurextras.modules;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.BrushableBlock;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
+import org.purpurmc.purpurextras.PurpurExtras;
+
+/**
+ * If enabled, players will be able to shift-right click on sand and gravel with items in their hands to create
+ * suspicious blocks and put held item inside. Held item will disappear from player's hand and will be added as loot
+ * inside the suspicious block. Only one item can be added per block.
+ */
+public class CreateSusBlocksModule implements PurpurExtrasModule, Listener {
+
+    protected CreateSusBlocksModule() {}
+
+    @Override
+    public void enable() {
+        PurpurExtras plugin = PurpurExtras.getInstance();
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    }
+
+    @Override
+    public boolean shouldEnable() {
+        return PurpurExtras.getPurpurConfig().getBoolean("settings.create-suspicious-blocks", false);
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+    public void onInteractWithSusBlock(PlayerInteractEvent event) {
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+        Player player = event.getPlayer();
+        if (!player.isSneaking()) return;
+        Block block = event.getClickedBlock();
+        if (block == null) return;
+        if (block.getType() != Material.SAND && block.getType() != Material.GRAVEL) return;
+        ItemStack itemStack = event.getItem();
+        if (itemStack == null) return;
+
+        switch (block.getType()) {
+            case SAND -> block.setType(Material.SUSPICIOUS_SAND);
+            case GRAVEL -> block.setType(Material.SUSPICIOUS_GRAVEL);
+            default -> {
+                return;
+            }
+        }
+
+        BlockState blockState = block.getState();
+        if (!(blockState instanceof BrushableBlock brushableBlock)) return;
+        brushableBlock.setItem(itemStack.asOne());
+        event.getItem().setAmount(itemStack.getAmount() - 1);
+        brushableBlock.update();
+        event.setUseInteractedBlock(Event.Result.DENY);
+        event.setUseItemInHand(Event.Result.DENY);
+
+        EquipmentSlot hand = event.getHand();
+        if (hand == null) return;
+        player.swingHand(hand);
+    }
+}
