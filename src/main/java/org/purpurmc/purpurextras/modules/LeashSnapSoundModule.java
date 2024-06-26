003f781c8f0874e3a68b77746907f8410f9e6dd1
doom <61824552+JustDoom@users.noreply.github.com>
2024-06-26 19:04:49 +1000

    Support custom sounds

--- src/main/java/org/purpurmc/purpurextras/modules/LeashSnapSoundModule.java
+++ src/main/java/org/purpurmc/purpurextras/modules/LeashSnapSoundModule.java
@@ -3,6 +3,7 @@ package org.purpurmc.purpurextras.modules;
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.sound.Sound;
 import org.bukkit.NamespacedKey;
+import org.bukkit.SoundCategory;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -16,20 +17,12 @@ import java.util.Locale;
  */
 public class LeashSnapSoundModule implements PurpurExtrasModule, Listener {
 
-    private Key sound;
+    private String sound;
     private double volume;
     private double pitch;
 
     protected LeashSnapSoundModule() {
-        String soundId = PurpurExtras.getPurpurConfig().getString("settings.leash-snap.sound", "block.bamboo.break");
-
-        try {
-            sound = org.bukkit.Sound.valueOf(soundId.replace('.', '_').toUpperCase(Locale.ROOT)).key();
-        } catch (IllegalArgumentException e) {
-            PurpurExtras.getInstance().getLogger().warning("Could not set sound to '" + soundId + "', using default value 'block.bamboo.break'");
-            sound = Key.key(Key.MINECRAFT_NAMESPACE, "block.bamboo.break");
-        }
-
+        sound = PurpurExtras.getPurpurConfig().getString("settings.leash-snap.sound", "minecraft:block.bamboo.break");
         volume = PurpurExtras.getPurpurConfig().getDouble("settings.leash-snap.volume", 1f);
         pitch = PurpurExtras.getPurpurConfig().getDouble("settings.leash-snap.pitch", 1.25f);
     }
@@ -50,6 +43,6 @@ public class LeashSnapSoundModule implements PurpurExtrasModule, Listener {
 
         if (event.getReason() != EntityUnleashEvent.UnleashReason.DISTANCE) return;
 
-        event.getEntity().getWorld().playSound(Sound.sound(sound, Sound.Source.PLAYER, (float) volume, (float) pitch), event.getEntity());
+        event.getEntity().getWorld().playSound(event.getEntity().getLocation(), sound, SoundCategory.PLAYERS, (float) volume, (float) pitch);
     }
 }
