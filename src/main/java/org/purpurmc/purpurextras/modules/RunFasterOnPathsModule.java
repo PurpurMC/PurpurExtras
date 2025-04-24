37906cec6e805fa14b7486ab20a15a35339f9539
YouHaveTrouble <youhavetrouble@youhavetrouble.me>
2025-04-24 20:35:12 +0200

    move from potion effects to transient attributes

--- src/main/java/org/purpurmc/purpurextras/modules/RunFasterOnPathsModule.java
+++ src/main/java/org/purpurmc/purpurextras/modules/RunFasterOnPathsModule.java
@@ -1,14 +1,16 @@
 package org.purpurmc.purpurextras.modules;
 
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
+import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 import org.purpurmc.purpurextras.PurpurConfig;
 import org.purpurmc.purpurextras.PurpurExtras;
 
@@ -25,9 +27,8 @@ import java.util.logging.Logger;
 public class RunFasterOnPathsModule implements PurpurExtrasModule, Listener {
 
     private final HashSet<Material> pathBlocks = new HashSet<>();
-    private final int speedMultiplier;
-
-    private final PotionEffect speedEffect;
+    private final AttributeModifier modifier;
+    private final NamespacedKey attributeKey = new NamespacedKey(PurpurExtras.getInstance(), "run-faster-on-paths");
 
     protected RunFasterOnPathsModule() {
         List<String> defaults = new ArrayList<>();
@@ -36,11 +37,18 @@ public class RunFasterOnPathsModule implements PurpurExtrasModule, Listener {
         PurpurConfig config = PurpurExtras.getPurpurConfig();
         Logger logger = PurpurExtras.getInstance().getLogger();
 
-        int rawSpeedMultiplier = config.getInt("settings.gameplay-settings.run-faster-on-paths.speed-multiplier", 0);
-        speedMultiplier = Math.max(0, rawSpeedMultiplier);
-        int duration = config.getInt("settings.gameplay-settings.run-faster-on-paths.duration", 2);
+        String attributeModifierType = config.getString("settings.gameplay-settings.run-faster-on-paths.attribute-modifier-type", "add_scalar");
+        AttributeModifier.Operation operation;
+        try {
+            operation = AttributeModifier.Operation.valueOf(attributeModifierType.toUpperCase(Locale.ENGLISH));
+        } catch (IllegalArgumentException e) {
+            logger.warning(attributeModifierType + " is not a valid attribute modifier type. Defaulting to add_scalar.");
+            operation = AttributeModifier.Operation.ADD_SCALAR;
+        }
 
-        speedEffect = new PotionEffect(PotionEffectType.SPEED, duration, Math.max(speedMultiplier - 1, 0), false, false, false);
+        double value = config.getDouble("settings.gameplay-settings.run-faster-on-paths.value", 0d);
+
+        modifier = new AttributeModifier(attributeKey, value, operation);
 
         List<String> rawPathBlocks = config.getList("settings.gameplay-settings.run-faster-on-paths.path-blocks", defaults);
         rawPathBlocks.forEach((string) -> {
@@ -61,21 +69,37 @@ public class RunFasterOnPathsModule implements PurpurExtrasModule, Listener {
 
     @Override
     public boolean shouldEnable() {
-        return speedMultiplier > 0;
+        PurpurConfig config = PurpurExtras.getPurpurConfig();
+        return config.getBoolean("settings.gameplay-settings.run-faster-on-paths.enabled", false);
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
     public void onPlayerMoveOnPath(PlayerMoveEvent event) {
-        if (!event.hasChangedPosition()) return;
+        if (!event.hasChangedBlock()) return;
         Player player = event.getPlayer();
         if (player.isFlying()) return;
         if (player.isGliding()) return;
         if (player.isInsideVehicle()) return;
 
         Block block = player.getLocation().clone().subtract(0.0, 0.1, 0.0).getBlock();
-        if (!pathBlocks.contains(block.getType())) return;
+        AttributeInstance attributeInstance = player.getAttribute(Attribute.MOVEMENT_SPEED);
+        if (attributeInstance == null) return;
+        if (!pathBlocks.contains(block.getType())) {
+            attributeInstance.removeModifier(attributeKey);
+            return;
+        }
 
-        player.addPotionEffect(speedEffect);
+        AttributeModifier existingModifier = attributeInstance.getModifier(attributeKey);
+
+        if (existingModifier == null) {
+            attributeInstance.addTransientModifier(this.modifier);
+            return;
+        }
+
+        if (!existingModifier.getOperation().equals(this.modifier.getOperation()) && existingModifier.getAmount() != this.modifier.getAmount()) {
+            attributeInstance.removeModifier(existingModifier);
+            attributeInstance.addTransientModifier(this.modifier);
+        }
     }
 
 }
