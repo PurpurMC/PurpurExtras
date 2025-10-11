653b2035e3e0c341c3bfbcfeb9851b5fb32fc9f4
Rhythmic <45906780+RhythmicSys@users.noreply.github.com>
2025-10-11 10:54:59 -0700

    Moved command to brigadier, changes over to a paper-plugin (#103)
    * Moved command to brigadier, changes over to a paper-plugin
    
    * Change requested changes

--- src/main/java/org/purpurmc/purpurextras/PurpurExtrasCommand.java
+++ src/main/java/org/purpurmc/purpurextras/PurpurExtrasCommand.java
@@ -1,53 +1,51 @@
 package org.purpurmc.purpurextras;
 
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.text.format.TextDecoration;
-import org.bukkit.command.Command;
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.tree.LiteralCommandNode;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.command.brigadier.Commands;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.TabExecutor;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import org.bukkit.permissions.Permission;
 
-import java.util.Collections;
-import java.util.List;
+public class PurpurExtrasCommand {
 
-public class PurpurExtrasCommand implements TabExecutor {
-    @Override
-    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+    private static final String reloadCommand = "reload";
+    private static final Permission reloadPermission = new Permission("purpurextras.reload");
+    private static final String versionCommand = "version";
+    private static final String purpurExtrasCommand = "purpurextras";
 
-        if (strings.length == 0) {
-            commandSender.sendMessage(
-                    Component.text("PurpurExtras", NamedTextColor.DARK_PURPLE, TextDecoration.BOLD)
-                    .append(Component.text(" by YouHaveTrouble"))
-            );
-        }
+    public static LiteralCommandNode<CommandSourceStack> createCommand() {
+        return Commands.literal(purpurExtrasCommand)
+                .requires(css -> css.getSender().hasPermission("purpurextras.command"))
+                .executes(PurpurExtrasCommand::executeDefault)
+                .then(Commands.literal(reloadCommand)
+                        .requires(css -> css.getSender().hasPermission(reloadPermission))
+                        .executes(PurpurExtrasCommand::executeReload))
+                .then(Commands.literal(versionCommand)
+                        .executes(PurpurExtrasCommand::executeVersion))
+                .build();
 
-        if (strings.length == 1 && strings[0].equalsIgnoreCase("reload")) {
-            if (!commandSender.hasPermission("purpurextras.reload")) {
-                commandSender.sendMessage(Component.text("You don't have permission to do that.", NamedTextColor.RED));
-                return true;
-            }
-            commandSender.sendMessage(Component.text("Reloading PurpurExtras config..."));
-            PurpurExtras.getInstance().reloadPurpurExtrasConfig(commandSender);
-            return true;
-        }
-
-        if (strings.length == 1 && strings[0].equalsIgnoreCase("version")) {
-            commandSender.sendMessage(Component.text("PurpurExtras version "+PurpurExtras.getInstance().getDescription().getVersion()));
-        }
-
-        return true;
     }
 
 
-    @Override
-    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, @NotNull String[] args) {
-        List<String> cmds = List.of("reload", "version");
-        if(args.length == 1) {
-            return cmds.stream().filter(s -> s.toLowerCase().startsWith(args[0])).toList();
-        }else {
-            return Collections.emptyList();
-        }
+    public static int executeDefault(CommandContext<CommandSourceStack> ctx) {
+        //I am making this fancier because its cooler
+        ctx.getSource().getSender().sendRichMessage("<gradient:#8d54ff:#adf3ff><b>PurpurExtras</b></gradient> <white>by the Purpur Team");
+        return Command.SINGLE_SUCCESS;
     }
+
+
+    public static int executeVersion(CommandContext<CommandSourceStack> ctx) {
+        ctx.getSource().getSender().sendRichMessage("<gradient:#8d54ff:#adf3ff><b>PurpurExtras</b></gradient> version: " + PurpurExtras.getInstance().getDescription().getVersion());
+        return Command.SINGLE_SUCCESS;
+    }
+
+    public static int executeReload(CommandContext<CommandSourceStack> ctx) {
+        CommandSender sender = ctx.getSource().getSender();
+        sender.sendRichMessage("Reloading PurpurExtras config...");
+        PurpurExtras.getInstance().reloadPurpurExtrasConfig(sender);
+        return Command.SINGLE_SUCCESS;
+    }
+
 }
