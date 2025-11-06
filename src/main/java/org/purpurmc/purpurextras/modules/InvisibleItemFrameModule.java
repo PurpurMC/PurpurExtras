40e0a07f9a2978bdb975b8e76448e1d584a0f123
Rhythmic System <mc.adhdmc@gmail.com>
2025-11-05 16:40:54 -0800

    Adjusts item frames to allow left or right click, using LEFT as default.

--- src/main/java/org/purpurmc/purpurextras/modules/InvisibleItemFrameModule.java
+++ src/main/java/org/purpurmc/purpurextras/modules/InvisibleItemFrameModule.java
@@ -1,5 +1,6 @@
 package org.purpurmc.purpurextras.modules;
 
+import io.papermc.paper.event.player.PrePlayerAttackEntityEvent;
 import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.ItemFrame;
@@ -7,10 +8,12 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.permissions.Permission;
 import org.bukkit.permissions.PermissionDefault;
+import org.purpurmc.purpurextras.PurpurConfig;
 import org.purpurmc.purpurextras.PurpurExtras;
 
 /**
@@ -19,7 +22,17 @@ import org.purpurmc.purpurextras.PurpurExtras;
  */
 public class InvisibleItemFrameModule implements PurpurExtrasModule, Listener {
 
-    protected InvisibleItemFrameModule() {}
+    private ToggleClickType clickType;
+
+    protected InvisibleItemFrameModule() {
+        String clickString = PurpurExtras.getPurpurConfig().getString("settings.blocks.invisible-frames.click-type", "LEFT");
+        try {
+            clickType = ToggleClickType.valueOf(clickString.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            PurpurExtras.getInstance().getSLF4JLogger().warn("{} is not a valid choice for click type! Please use the values 'RIGHT' or 'LEFT'! Defaulting to 'LEFT'", clickString);
+            clickType = ToggleClickType.LEFT;
+        }
+    }
 
     private final Permission invisFramePermission = new Permission(
             "purpurextras.invisibleframes",
@@ -36,23 +49,75 @@ public class InvisibleItemFrameModule implements PurpurExtrasModule, Listener {
     @Override
     public boolean shouldEnable() {
         registerPermissions(invisFramePermission);
-        return PurpurExtras.getPurpurConfig().getBoolean("settings.blocks.shift-right-click-for-invisible-item-frames", false);
+        PurpurConfig config = PurpurExtras.getPurpurConfig();
+        migrateConfig(config);
+        return config.getBoolean("settings.blocks.invisible-frames.enabled", false);
     }
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onItemFrameInteract(PlayerInteractEntityEvent event){
+    public void onItemFrameInteract(PlayerInteractEntityEvent event) {
         if (event.getHand().equals(EquipmentSlot.OFF_HAND)) return;
+        if (clickType.equals(ToggleClickType.LEFT)) return;
 
         Player player = event.getPlayer();
         if (!player.isSneaking()) return;
 
         Entity entity = event.getRightClicked();
         if (!(entity instanceof ItemFrame itemFrame)) return;
-        if (itemFrame.getItem().getType().equals(Material.AIR)) return;
-        if (!player.hasPermission(invisFramePermission)) return;
-
+        if (!passesChecks(player, itemFrame)) return;
         event.setCancelled(true);
+        toggleFrame(itemFrame);
+    }
+
+
+    /**
+     * This handles the attack when the item frame is invisible.
+     * FOR SOME REASON the other event doesn't fire when the item frame is hit in this state
+     */
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onItemFrameAttack(EntityDamageByEntityEvent event) {
+        if (clickType.equals(ToggleClickType.RIGHT)) return;
+        if (!(event.getEntity() instanceof ItemFrame itemFrame)) return;
+        if (!(event.getDamager() instanceof Player player)) return;
+        if (!player.isSneaking()) return;
+        if (!passesChecks(player, itemFrame)) return;
+        event.setCancelled(true);
+        toggleFrame(itemFrame);
+    }
+
+    /**
+     * Handles when the item frame is invisible, 'cause apparently it can't handle the visible one as well for some reason.
+     */
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onItemFrameAttack(PrePlayerAttackEntityEvent event) {
+        Player player = event.getPlayer();
+        if (!player.isSneaking()) return;
+        if (clickType.equals(ToggleClickType.RIGHT)) return;
+        if (!(event.getAttacked() instanceof ItemFrame itemFrame)) return;
+        if (!passesChecks(player, itemFrame)) return;
+        event.setCancelled(true);
+        toggleFrame(itemFrame);
+    }
+
+    private boolean passesChecks(Player player, ItemFrame itemFrame) {
+        if (itemFrame.getItem().getType().equals(Material.AIR)) return false;
+        return player.hasPermission(invisFramePermission);
+    }
+
+    private void toggleFrame(ItemFrame itemFrame) {
         itemFrame.setVisible(!itemFrame.isVisible());
         itemFrame.setFixed(!itemFrame.isFixed());
+    }
+
+    public enum ToggleClickType {
+        LEFT,
+        RIGHT;
+    }
+
+    private void migrateConfig(PurpurConfig config) {
+        boolean previousValue = config.getBooleanAndRemove("settings.blocks.shift-right-click-for-invisible-item-frames", false);
+        config.getBoolean("settings.blocks.invisible-frames.enabled", previousValue);
     }
 }
