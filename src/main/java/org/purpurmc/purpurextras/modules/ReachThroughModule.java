b6dc5c232cc20ffb124471dd56cb8d67b4e196ad
Rhythmic System <mc.adhdmc@gmail.com>
2025-11-08 16:23:28 -0800

    Implements piglin Aggro, statistic incrementation, and respecting blocked containers

--- src/main/java/org/purpurmc/purpurextras/modules/ReachThroughModule.java
+++ src/main/java/org/purpurmc/purpurextras/modules/ReachThroughModule.java
@@ -2,15 +2,13 @@ package org.purpurmc.purpurextras.modules;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
-import org.bukkit.block.Container;
-import org.bukkit.block.Sign;
+import org.bukkit.Material;
+import org.bukkit.Statistic;
+import org.bukkit.block.*;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.Directional;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.ItemFrame;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.*;
+import org.bukkit.entity.memory.MemoryKey;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -22,11 +20,16 @@ import org.bukkit.material.Attachable;
 import org.purpurmc.purpurextras.PurpurExtras;
 
 import java.util.HashSet;
+import java.util.List;
 
 public class ReachThroughModule implements PurpurExtrasModule, Listener {
-    private boolean bypassEmptyFrames, bypassFilledFrames, bypassUnwaxedSigns, bypassWaxedSigns, bypassPaintings;
+    private boolean bypassEmptyFrames, bypassFilledFrames, bypassUnwaxedSigns, bypassWaxedSigns, bypassPaintings,
+            openBlockedContainers, triggerPiglinAggro, incrementStatistics;
 
     private final HashSet<EntityType> passableEntities = new HashSet<>();
+
+    public ReachThroughModule() {
+        }
 
     @Override
     public void enable() {
@@ -37,6 +40,20 @@ public class ReachThroughModule implements PurpurExtrasModule, Listener {
             passableEntities.add(EntityType.GLOW_ITEM_FRAME);
         }
         if (bypassPaintings) passableEntities.add(EntityType.PAINTING);
+        openBlockedContainers = PurpurExtras.getPurpurConfig().getBoolean("settings.reach-through.options.open-blocked-containers", false);
+        triggerPiglinAggro = PurpurExtras.getPurpurConfig().getBoolean("settings.reach-through.options.trigger-piglin-aggro", true);
+        incrementStatistics = PurpurExtras.getPurpurConfig().getBoolean("settings.reach-through.options.increment-statistics", true);
+
+    }
+
+    @Override
+    public boolean shouldEnable() {
+        bypassEmptyFrames = PurpurExtras.getPurpurConfig().getBoolean("settings.reach-through.item-frames.empty", false);
+        bypassFilledFrames = PurpurExtras.getPurpurConfig().getBoolean("settings.reach-through.item-frames.filled", false);
+        bypassPaintings = PurpurExtras.getPurpurConfig().getBoolean("settings.reach-through.paintings", false);
+        bypassUnwaxedSigns = PurpurExtras.getPurpurConfig().getBoolean("settings.reach-through.signs.unwaxed", false);
+        bypassWaxedSigns = PurpurExtras.getPurpurConfig().getBoolean("settings.reach-through.signs.waxed", false);
+        return bypassEmptyFrames || bypassFilledFrames || bypassPaintings || bypassUnwaxedSigns || bypassWaxedSigns;
     }
 
     @EventHandler(ignoreCancelled = true)
@@ -52,9 +69,10 @@ public class ReachThroughModule implements PurpurExtrasModule, Listener {
         Container container = getContainer(face, entityClicked.getLocation().toBlockLocation());
         if (container == null) return;
         if (!canInteractWithContainer(player, container, face)) return;
+        if (!canOpenContainer(container)) return;
         interactEntityEvent.setCancelled(true);
         player.openInventory(container.getInventory());
-
+        handleContainerTypes(player, container);
     }
 
     @EventHandler(ignoreCancelled = true)
@@ -72,18 +90,10 @@ public class ReachThroughModule implements PurpurExtrasModule, Listener {
         Container container = getContainer(face, blockClicked.getLocation());
         if (container == null) return;
         if (!canInteractWithContainer(player, container, face)) return;
+        if (!canOpenContainer(container)) return;
         interactEvent.setCancelled(true);
         player.openInventory(container.getInventory());
-    }
-
-    @Override
-    public boolean shouldEnable() {
-        bypassEmptyFrames = PurpurExtras.getPurpurConfig().getBoolean("settings.reach-through.item-frames.empty", false);
-        bypassFilledFrames = PurpurExtras.getPurpurConfig().getBoolean("settings.reach-through.item-frames.filled", false);
-        bypassPaintings = PurpurExtras.getPurpurConfig().getBoolean("settings.reach-through.paintings", false);
-        bypassUnwaxedSigns = PurpurExtras.getPurpurConfig().getBoolean("settings.reach-through.signs.unwaxed", false);
-        bypassWaxedSigns = PurpurExtras.getPurpurConfig().getBoolean("settings.reach-through.signs.waxed", false);
-        return bypassEmptyFrames || bypassFilledFrames || bypassPaintings || bypassUnwaxedSigns || bypassWaxedSigns;
+        handleContainerTypes(player, container);
     }
 
     @SuppressWarnings("UnstableApiUsage")
@@ -94,6 +104,21 @@ public class ReachThroughModule implements PurpurExtrasModule, Listener {
         return !newEvent.useInteractedBlock().equals(Event.Result.DENY);
     }
 
+    private boolean canOpenContainer(Container container) {
+        if (openBlockedContainers) return true;
+        if (container instanceof ShulkerBox shulkerBox) {
+            Directional directional = (Directional) shulkerBox.getBlockData();
+            BlockFace blockFace = directional.getFacing();
+            Location facingBlock = shulkerBox.getLocation().add(blockFace.getModX(), blockFace.getModY(), blockFace.getModZ()).toBlockLocation();
+            return facingBlock.getBlock().getType().isAir();
+        }
+        if (container instanceof Chest) {
+            Location location = container.getLocation().add(0, 1, 0);
+            return !location.getBlock().isSuffocating();
+        }
+        return true;
+    }
+
     private Container getContainer(BlockFace face, Location location) {
         int attachedXOffset = face.getModX();
         int attachedYOffset = face.getModY();
@@ -102,6 +127,66 @@ public class ReachThroughModule implements PurpurExtrasModule, Listener {
         Block blockAtLocation = attachedBlockLocation.getBlock();
         if (!(blockAtLocation.getState() instanceof Container containerBlock)) return null;
         return containerBlock;
+    }
+
+    private void aggroPiglins(Player player) {
+        List<Entity> nearbyEntities = player.getNearbyEntities(15, 3, 15);
+        for (Entity entity : nearbyEntities) {
+            if (!(entity instanceof Piglin piglin)) continue;
+            if (!piglin.hasLineOfSight(player)) continue;
+            piglin.setMemory(MemoryKey.ANGRY_AT, player.getUniqueId());
+        }
+    }
+
+
+    private void handleContainerTypes(Player player, Container container) {
+        Block block = container.getBlock();
+        if (block.getType().equals(Material.TRAPPED_CHEST)) {
+            if (triggerPiglinAggro) aggroPiglins(player);
+            if (incrementStatistics) player.incrementStatistic(Statistic.TRAPPED_CHEST_TRIGGERED);
+        }
+        BlockData blockData = container.getBlock().getBlockData();
+        if (blockData instanceof EnderChest) if (incrementStatistics) player.incrementStatistic(Statistic.ENDERCHEST_OPENED);
+
+        switch (container) {
+            case Barrel ignored -> {
+                if (triggerPiglinAggro) aggroPiglins(player);
+                if (incrementStatistics) player.incrementStatistic(Statistic.OPEN_BARREL);
+            }
+            case BlastFurnace ignored -> {
+                if (incrementStatistics) player.incrementStatistic(Statistic.INTERACT_WITH_BLAST_FURNACE);
+            }
+            case BrewingStand ignored -> {
+                if (incrementStatistics) player.incrementStatistic(Statistic.BREWINGSTAND_INTERACTION);
+            }
+            case Chest ignored -> {
+                if (triggerPiglinAggro) aggroPiglins(player);
+                if (incrementStatistics) player.incrementStatistic(Statistic.CHEST_OPENED);
+            }
+            case Crafter ignored -> {
+                //There is no statistic for this??
+            }
+            case Dispenser ignored -> {
+                if (incrementStatistics) player.incrementStatistic(Statistic.DISPENSER_INSPECTED);
+            }
+            case Dropper ignored -> {
+                if (incrementStatistics) player.incrementStatistic(Statistic.DROPPER_INSPECTED);
+            }
+            case Smoker ignored -> {
+                if (incrementStatistics) player.incrementStatistic(Statistic.INTERACT_WITH_SMOKER);
+            }
+            case Furnace ignored -> {
+                if (incrementStatistics) player.incrementStatistic(Statistic.FURNACE_INTERACTION);
+            }
+            case Hopper ignored -> {
+                if (incrementStatistics) player.incrementStatistic(Statistic.HOPPER_INSPECTED);
+            }
+            case ShulkerBox ignored -> {
+                if (triggerPiglinAggro) aggroPiglins(player);
+                if (incrementStatistics) player.incrementStatistic(Statistic.SHULKER_BOX_OPENED);
+            }
+            default -> {}
+        }
     }
 
     private boolean signPassthroughEnabled(Sign sign) {
